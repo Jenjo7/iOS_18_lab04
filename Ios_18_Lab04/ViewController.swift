f4adefb179ec7f3aaf16ec39c3cb3aaf275713ad
Gianni Savini <gianni.savini@studio.unibo.it>
2018-05-24 14:09:13 +0200

    Initial Commit

---
+++ Ios_18_Lab04/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ios_18_Lab04
+//
+//  Created by Gianni Savini on 24/05/18.
+//  Copyright © 2018 Gianni Savini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
